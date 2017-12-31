5b65f9564c94e132ae698537ea7d5ab284a17933
hitoabe <hitoabe@yahoo-corp.jp>
2018-01-01 01:26:31 +0900

    data 2017

--- src/total_average.py
+++ src/total_average.py
@@ -9,16 +9,16 @@ from controller import Controller
 
 def main():
     # Set Holds
-    holds = {'BTC':0.5, 'BCH':3.0, 'ZAIF':50.5}
+    holds = {'BTC':0.0001, 'BCH':45.37983532, 'ZAIF':2154694.4}
     # Read JPY/USD, BTC/JPY, BCH/JPY
     jpy = JPY('../data/2017/jpy/USDJPY.csv', '../data/2017/jpy/btc-jpy-max.csv', '../data/2017/jpy/bch-jpy-max.csv')
     # Join JPY Data When Read History of Each Exchange
     # Read Trade (and Withdraw) History of Bitfinex
-    bitfinex = BitFinex('bitfinex', '../data/2017/bitfinex/trades.csv', jpy)
+    bitfinex = BitFinex('bitfinex', '../data/2017/bitfinex/2017-12-28-trades.csv', jpy)
     # Read Trade (and Withdraw) History of Cryptopia
-    cryptopia = Cryptopia('cryptopia', '../data/2017/cryptopia/trade.csv', '../data/2017/cryptopia/withdraw.csv', jpy)
+    cryptopia = Cryptopia('cryptopia', '../data/2017/cryptopia/Trade_History.csv', '../data/2017/cryptopia/Withdraw_History.csv', jpy)
     # Read Trade (and Withdraw) History of Bitflyer
-    bitflyer = BitFlyer('bitflyer', '../data/2017/bitflyer/trade.csv', '../data/2017/bitflyer/withdraw.csv', jpy)
+    bitflyer = BitFlyer('bitflyer', '../data/2017/bitflyer/TradeHistory_20171228.csv', '../data/2017/bitflyer/Deposit_Withdraw_History_20171228.csv', jpy)
     # Read Trade (and Withdraw) History of Zaif
     zaif = Zaif('zaif', '../data/2017/zaif/trade.csv', '../data/2017/zaif/withdraw.csv', jpy)
     # Init Controller
