39f494d0e9736b4d4c362ac547a3fc5e9bd720de
hitoabe <hitoabe@yahoo-corp.jp>
2017-12-31 23:52:47 +0900

    f

--- src/zaif.py
+++ src/zaif.py
@@ -19,12 +19,12 @@ class Zaif(Exchange):
     def setTrades(self, f_trades, jpy):
         self.trades = pd.read_csv(f_trades, parse_dates=['日時'])
         self.trades['日時'] += pd.offsets.BDay(normalize=True)
-        self.trades = pd.merge(self.trades, jpy.getBTC(), left_on='日時', right_on='snapped_at', how='left')
+        self.trades = pd.merge(self.trades, jpy.getJPY(), left_on='日時', right_on='snapped_at', how='left')
 
     def setWithdraws(self, f_withdraws, jpy):
         self.withdraws = pd.read_csv(f_withdraws, parse_dates=['日時'])
         self.withdraws['日時'] += pd.offsets.BDay(normalize=True)
-        self.withdraws = pd.merge(self.withdraws, jpy.getBTC(), left_on='日時', right_on='snapped_at', how='left')
+        self.withdraws = pd.merge(self.withdraws, jpy.getJPY(), left_on='日時', right_on='snapped_at', how='left')
 
     def calcResult(self):
         result = {'Fee':0.0}
